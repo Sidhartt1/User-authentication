a3060c46de5d1abd3acb4d08c90c3ba82a51ad9a
Sidhartt1 <sidhsidhad@gmail.com>
2022-09-05 13:59:47 +0530

    phase 3 pat 3

---
+++ userAuthentication/src/main/java/com/example/demo/config/webSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class webSecurityConfig 
+{
+	
+}
